e88a6885c2b05c4c30bb5ef599334898760b412d
HemingwayLee <coherence0815@gmail.com>
2024-05-01 21:09:25 +0800

    add examples

--- fileConverter/hello_heic.py
+++ fileConverter/hello_heic.py
@@ -1,19 +1,28 @@
 from PIL import Image
 import os
+import pillow_heif
+import numpy as np
+import cv2
+
+# from pillow_heif import register_heif_opener
+
+# register_heif_opener()
 
 # folder_path = "/Users/rosemary/Downloads/"
 
 folder_path = "/Users/rosemary/Desktop/"
 for filename in os.listdir(folder_path):
-  if filename.endswith(".png"):
+  if filename.endswith(".HEIC"):
     input_filepath = os.path.join(folder_path, filename)
     output_filepath = os.path.splitext(input_filepath)[0] + ".jpg"  # Change extension
 
     try:
-      image = Image.open(input_filepath)
-      image = image.convert('RGB')
-      image.save(output_filepath, quality=95)
-
+      heif_file = pillow_heif.open_heif(input_filepath)
+      # image = image.convert('RGB')
+      # image.save(output_filepath, quality=95)
+      
+      np_array = np.asarray(heif_file)
+      cv2.imwrite(output_filepath, np_array)
       print(f"Converted {filename} to JPEG")
       os.remove(input_filepath)
 
